4154a44cca56751e4ec775da321f6aac93afa88e
raviolican <daumsimon94@gmail.com>
2016-07-22 22:29:37 +0200

    Refined Object Orientation
    Refined object orientation.
    Created packages
    Added some Docstring
    Cleaned code

--- mods/__init__.py
+++ mods/__init__.py
@@ -1,8 +1,5 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
-# Prints the ID of any instance
-def getIds(objects):
-    for i in objects:
-        print(i,id(i))
+from .htmlbuilder import HtmlBuilder
+from .tablegen import TableGen
